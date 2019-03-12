96ee9bac9afb98b5c111bff0efd8b068423dda72
Your Name <email@example.com>
2019-03-12 15:47:48 +0800

    获取地区

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var dateutils = require('../public/javascripts/dateutils')
 var router = express.Router();
 var moment = require('moment');
-/* GET home page. */
-router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Express' });
-});
+var pcasv = require('../public/javascripts/pcasv');
 
-router.get('/jk', function (req, res, next) {
-	let date = new Date();
-	date.setHours(date.getHours() + 2);
-	let data = {
-		time: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
-	}
-	res.json(data);
+
+router.get('/addres', function (req, res, next) {
+	let pro = req.query.pro || ""
+	pcasv.forEach(element => {
+		if (element.value === pro) {
+			res.json(element.children);
+		}
+	});
+	res.json({});	
 });
+/* GET home page. */
 router.get('/', function (req, res, next) {
 	let date = { title: 'moment日期格式化时间', time: moment().format('YYYY-MM-DD HH:mm:ss') }
 	res.json(date);
