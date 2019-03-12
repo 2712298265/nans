8ef60559f92f621358a792880fe40b7476cf4856
2712298265 <2712298265@qq.com>
2019-03-12 22:51:35 +0800

    获取省市区接口

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,52 @@
 var express = require('express');
-var dateutils = require('../public/javascripts/dateutils')
 var router = express.Router();
 var moment = require('moment');
 var pcasv = require('../public/javascripts/pcasv');
 
-
+/**
+ * 获取省市区
+ */
 router.get('/addres', function (req, res, next) {
-	let pro = req.query.pro || ""
-	pcasv.forEach(element => {
-		if (element.value === pro) {
-			res.json(element.children);
+	let pro = req.query.pro
+	let result = {
+		status: 0,
+		msg: "查询成功！",
+		data: {}
+	}
+	let array = []
+	pcasv.forEach(province => { //循环省
+		if (province.value === pro) {
+			array.push(province)
 		}
+		province.children.forEach( //循环市
+			city => {
+				if (city.value === pro) {
+					array.push(city)
+				}
+				city.children.forEach( //循环区
+					area => {
+						if (area.value === pro) {
+							array.push(area)
+						}
+					}
+				)
+			}
+		)
 	});
-	res.json({});	
+	if (array.length > 0) {
+		result.data = array;
+	}
+	res.json(result);
 });
-/* GET home page. */
-router.get('/', function (req, res, next) {
-	let date = { title: 'moment日期格式化时间', time: moment().format('YYYY-MM-DD HH:mm:ss') }
+
+/**
+ * 获取格式话日期
+ */
+router.get('/getformatdate', function (req, res, next) {
+	let date = {
+		title: 'moment日期格式化时间',
+		time: moment().format('YYYY-MM-DD HH:mm:ss')
+	}
 	res.json(date);
 });
 
